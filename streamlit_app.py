a3546a13302374883ad7c6ecea04c380115585a8
ykipu <166149735+ykipu@users.noreply.github.com>
2024-07-26 14:00:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from keras.model import load_model
+from keras.models import load_model
 import matplotlib.pyplot as plt
 import yfinance as yf
 
