756aac9b6508c17eef977a9919987ce13af19896
ykipu <166149735+ykipu@users.noreply.github.com>
2024-07-26 13:58:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,29 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from keras.model import load_model
+import matplotlib.pyplot as plt
+import yfinance as yf
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Stock Price Predicitior App")
+
+st.text_input("Enter the stock ID", 'NVDA')
+
+from datetime import datetime
+end= datetime.now()
+start= datetime(end.year-20,end.monyh,end.day)
+
+nvidia_data = yf.download(stock, start, end)
+
+model = looad_model("Latest_stock_price_model.keras")
+st.subheader("stock Data")
+st.write(nvidia_data)
+
+splittinf_len = int(len(nvidia_data)*0.7)
+x_test = pd.DataFrame(nvidia_dat.clode[splittin_len:])
+
+def plot_graph(figsize, values, full_data):
+    fig= plt.figure(figsize=figsize)
+    plt.plot(values,'Orange')
+    plt.plot(full_data.close, 'b')
+    return fig
